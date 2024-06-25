e86a9cc8bb4663c916e7011ef388d04c6c80d6de
Meandr58 <andrey.mescherin@gmail.com>
2024-06-26 01:24:20 +0300

    result

--- main.py
+++ main.py
@@ -9,43 +9,62 @@ def add_task():
 def delete_task():
     selected_task = task_listBox.curselection()
     if selected_task:
-        task_listBox.delete(selected_task)
+        task_listBox.delete(selected_task[0])
+    else:
+        selected_task = completed_listBox.curselection()
+        if selected_task:
+            completed_listBox.delete(selected_task[0])
 
 def mark_task():
     selected_task = task_listBox.curselection()
     if selected_task:
-        task_listBox.itemconfig(selected_task, bg="slate blue")
+        task = task_listBox.get(selected_task)
+        task_listBox.delete(selected_task)
+        completed_listBox.insert(tk.END, task)
 
 root = tk.Tk()
 root.title("Task list")
-root.configure(background="HotPink")
+root.configure(background="chocolate1")
+root.geometry("600x600")
 
 # Заголовок поля ввода задачи
-text1 = tk.Label(root, text="Введите вашу задачу:", bg="HotPink")
+text1 = tk.Label(root, text="Введите вашу задачу:", bg="chocolate1")
 text1.pack(pady=5)
 
 # Ввод задачи
-task_entry = tk.Entry(root, width=30, bg="DeepPink1")
+task_entry = tk.Entry(root, width=80, bg="LightPink1")
 task_entry.pack(pady=10)
 
+# Фрейм для кнопок
+button_frame = tk.Frame(root, bg="chocolate1")
+button_frame.pack(pady=5)
+
 # Кнопка "Добавить задачу"
-add_task_button = tk.Button(root, text="Добавить задачу", command=add_task)
-add_task_button.pack(pady=5)
+add_task_button = tk.Button(button_frame, text="Добавить задачу", command=add_task)
+add_task_button.pack(side=tk.LEFT, padx=5)
 
 # Кнопка удаления задачи
-delete_button = tk.Button(root, text="Удалить задачу", command=delete_task)
-delete_button.pack(pady=5)
+delete_button = tk.Button(button_frame, text="Удалить задачу", command=delete_task)
+delete_button.pack(side=tk.LEFT, padx=5)
 
 # Кнопка выполненной задачи
-mark_button = tk.Button(root, text="Отметить выполненную задачу", command=mark_task)
-mark_button.pack(pady=5)
+mark_button = tk.Button(button_frame, text="Выполнено", command=mark_task)
+mark_button.pack(side=tk.LEFT, padx=5)
 
-# Заголовок списка задач
-text2 = tk.Label(root, text="Список задач:", bg="HotPink")
+# Заголовок списка актуальных задач
+text2 = tk.Label(root, text="Актуальные задачи", bg="chocolate1")
 text2.pack(pady=5)
 
 # Список добавленных задач
-task_listBox = tk.Listbox(root, height=10, width=50, bg="LightPink1")
+task_listBox = tk.Listbox(root, height=10, width=80, bg="LightPink1")
 task_listBox.pack(pady=10)
+
+# Заголовок списка выполненных задач
+text3 = tk.Label(root, text="Выполненные задачи", bg="chocolate1")
+text3.pack(pady=5)
+
+# Список выполненных задач
+completed_listBox = tk.Listbox(root, height=10, width=80, bg="LightPink1")
+completed_listBox.pack(pady=10)
 
 root.mainloop()
